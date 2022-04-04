7e657a3f007e5afd1df8a517f782ebfd68f5d2e7
Columbus Dickson <cd07547@georgiasouthern.edu>
2022-04-04 02:31:44 -0400

    WIP after splitting the channels and it being 2am I can't think of the way to get the three channels back together with the weird data mismatch problem I'm having right now. I'm going to get back at this in the morning

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 from PIL import Image
 import numpy as np
+from PIL import ImageOps
 soonToSus = Image.open('notSus.png')
+
+## How Sus do you want it
+deltaSus = 10
 
 x = int(soonToSus.size[0]/5)
 y = int(soonToSus.size[1]/5)
 
-print(x,y)
-
+## PreSus
 soonToSus.show()
 
-sus = np.array(soonToSus.convert("L"))
+## B&W Sus
+susBW = np.array(soonToSus.convert("L"))
 
-susArray = np.array([[0,100,100,100,0],[100,100,200,200,0],[100,100,100,100,0],[0,100,0,100,0]])
+## Color Sus
+susR = np.empty([x,y],np.uint8)
+susR, susG, susB = Image.Image.split(soonToSus)
+r,g,b = soonToSus.split()
+
+## This Array is pretty sus
+susArray = np.multiply(np.array([[0,1,1,1,0],[1,1,2,2,0],[1,1,1,1,0],[0,1,0,1,0],[0,0,0,0,0]]),deltaSus)
 
 susMap = np.empty((x*5,y*5))
 print(susMap.shape)
 
-susMap[0] = np.tile(susArray[0],x)
-susMap[1] = np.tile(susArray[1],x)
-susMap[2] = np.tile(susArray[2],x)
-susMap[3] = np.tile(susArray[3],x)
+susMap = np.tile(susArray,(x,y))
+## Find the darkness of parts of the picture to be changed to something more sus
+HowSusCanWeSus = np.subtract(susMap,susBW)
 
-print(susArray)
+# Image.fromarray(HowSusCanWeSus).show()
 
-print(susMap)
+susR = np.subtract(HowSusCanWeSus,susR)
+susG = np.subtract(HowSusCanWeSus,susG)
+susB = np.subtract(HowSusCanWeSus,susB)
+# Image.fromarray(susR).show()
+# Image.fromarray(susG).show()
+# Image.fromarray(susB).show()
 
-Image.fromarray(susMap).show()
+# susR = Image.fromarray(susR)
+# susG = Image.fromarray(susG)
+# susB = Image.fromarray(susB)
+
+sus = Image.merge("RGB",(r,g,b))
+sus.show()
